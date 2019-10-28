886f0b3f5ed2006bc95e2e09450b9008f517da80
SolanoSSD <solanogalema@gmail.com>
2019-10-28 10:11:22 -0300

    first commit

---
+++ src/main/java/com/deathlink/tcca/TccaApplication.java
@@ -0,0 +1,13 @@
+package com.deathlink.tcca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deathlink/tcca/TccaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deathlink.tcca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
